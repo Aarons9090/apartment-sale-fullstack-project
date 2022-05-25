c3aea85f572439167d8e54da005d325a095d62a1
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-05-25 23:12:52 +0300

    apartment card content design

--- client/src/components/ApartmentCard.js
+++ client/src/components/ApartmentCard.js
@@ -1,22 +1,27 @@
 import "../styles/ApartmentCard.css"
 const ApartmentCard = ({ apartment }) => {
+    let price;
+    switch (apartment.price.length) {
+        case 5: price = `${apartment.price.substring(0, 2)} ${apartment.price.substring(2)}€`; break
+        case 6: price = `${apartment.price.substring(0, 3)} ${apartment.price.substring(3)}€`; break
+        case 7: price = `${apartment.price.substring(0, 1)} ${apartment.price.substring(1, 4)} ${apartment.price.substring(4)}€`; break
+        default: price = apartment.price
+    }
 
     return (
-        <div className="card">
-            <div className="image-container">
-                <img src="https://cdn.pixabay.com/photo/2016/01/15/19/55/house-1142297_960_720.jpg" alt="house" />
-            </div>
-            <div className="info-box">
-                <div className="price-data">
-                    <p>{apartment.price}</p>
+        <div className="card-area">
+            <div className="card">
+                <div className="image-container">
+                    <img src="https://cdn.pixabay.com/photo/2016/01/15/19/55/house-1142297_960_720.jpg" alt="house" />
                 </div>
-                <div className="address">
-                    <p>{apartment.address}</p>
+                <div className="info-box">
+                    <span className="address">{apartment.address} {apartment.city}</span>
+                    <span className="price">{price}</span>
+                    <span className="area">{apartment.area} m{<sup>2</sup>}</span>
                 </div>
             </div>
-
-
         </div>
+
     )
 }
 
