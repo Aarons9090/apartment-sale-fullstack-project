c93abc00098c75715f6144492dffe30e9f2f34ab
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-06-16 18:03:14 +0300

    home page responsive design

--- client/src/components/pages/homePage/HomePage.js
+++ client/src/components/pages/homePage/HomePage.js
@@ -7,9 +7,13 @@ const HomePage = () => {
             <div className="background-container">
                 <div className="text-box">
                 <div className="main-text">FIND YOU FOREVER HOME</div>
-                <span>AND START A NEW CHAPTER</span>
+                <span>AND START A NEW CHAPTER . . .</span>
                 </div>
-
+                <div className="button-row">
+                    <button className="primary-btn">Find my new home</button>
+                    <span className="middle-text">or</span>
+                    <button className="secondary-btn">Sell my apartment</button>
+                </div>
             </div>
         </div>
     )
