0e9ff0a8b77f17649b8ada006de6ec9bb9a68eb5
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-06-17 12:43:59 +0300

    added a mobile responsive popup sidebar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom"
 import logo from "../resources/logo.svg"
 import "../styles/NavigationBar.css"
+import { useState } from "react"
 
 const NavigationBar = () => {
+    const [expanded, setExpanded] = useState(false)
+    const close = () => setExpanded(false)
+
+    const sidebarStyle = { transform: expanded ? "translateX(0)" : "translateX(100%)"}
+    const buttonStyle =  { opacity: expanded ? "0" : "100%"}
+    const onBlur = event => {
+        if (!event.currentTarget.contains(event.relatedTarget)) {
+            close()
+        }
+    }
+
     return (
         <div className="nav-bar">
             <div className="logo-container">
@@ -11,6 +23,25 @@ const NavigationBar = () => {
             <div className="links">
                 <Link to="/">Home</Link>|<Link to="/search">Apartments</Link>|
                 <Link to="/">Sell apartment</Link>
+            </div>
+            <div className="popup-menu" tabIndex={0} onBlur={onBlur}>
+                
+                <div className="sidebar" style={sidebarStyle}>
+                    <div className="sidebar-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/search">Apartments</Link>
+                    <Link to="/">Sell apartment</Link>
+                    </div>
+                    
+                </div>
+                <button
+                style={buttonStyle}
+                    onClick={() => {
+                        setExpanded(!expanded)
+                    }}
+                >
+                    ≡
+                </button>
             </div>
         </div>
     )
