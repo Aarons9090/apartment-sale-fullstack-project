b555664502436bccfe8e8f05db13d4cca9476279
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-06-20 22:52:11 +0300

    added arrows for carousel

--- client/src/components/pages/homePage/components/Carousel.js
+++ client/src/components/pages/homePage/components/Carousel.js
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { useRef } from "react"
-import { Link, useNavigate } from "react-router-dom"
-
+import { useNavigate } from "react-router-dom"
+import "../../../../styles/Carousel.css"
+import arrow from "../../../../resources/carousel_arrow.svg"
 const Carousel = () => {
     const apartments = useSelector(state => state.apartments)
     const [currentApartment, setCurrent] = useState(null)
+    const [index, setIndex] = useState(0)
     const [dots, setDots] = useState([])
     const timerRef = useRef(null)
     const CAROUSEL_MAX_IMAGES = 5
     const navigate = useNavigate()
-    let index = 0
 
     useEffect(() => {
         if (apartments.length !== 0) {
-            timeout()
+            clearTimeout(timerRef.current)
             setCurrent(apartments[0])
             let dots_list = []
             for (let i = 0; i < CAROUSEL_MAX_IMAGES; i++) {
-                dots_list.push(<span key={i} className="dot" id={i}></span>)
+                if (i === 0) {
+                    dots_list.push(
+                        <span
+                            key={i}
+                            style={{ backgroundColor: "white" }}
+                            className="dot"
+                            id={i}
+                        ></span>
+                    )
+                } else {
+                    dots_list.push(<span key={i} className="dot" id={i}></span>)
+                }
             }
             setDots(dots_list)
         }
@@ -33,35 +45,41 @@ const Carousel = () => {
         }
     }, [])
 
-    const timeout = () => {
-        timerRef.current = setTimeout(() => {
-            if (apartments.length !== 0) {
-                // when leaving page, carousel doesnt render
-                const carousel = document.getElementById("images")
-                if (carousel) {
-                    carousel.style.transform = `translateX(${index * -100}%)`
-                    setCurrent(apartments[index])
+    const changeApartment = () => {
+        // when leaving page, carousel doesnt render
+        const carousel = document.getElementById("images")
+        if (carousel) {
+            console.log("change to", index)
+            carousel.style.transform = `translateX(${index * -100}%)`
+            setCurrent(apartments[index])
 
-                    // change indicator color
-                    const dotList = document.getElementsByClassName("dot")
-                    Array.prototype.forEach.call(dotList, dot => {
-                        if (Number(dot.id) !== index) {
-                            dot.style["backgroundColor"] = "dimgray"
-                        } else {
-                            dot.style["backgroundColor"] = "white"
-                        }
-                    })
-
-                    if (index === CAROUSEL_MAX_IMAGES - 1) {
-                        index = 0
-                    } else {
-                        index++
-                    }
+            // change indicator color
+            const dotList = document.getElementsByClassName("dot")
+            Array.prototype.forEach.call(dotList, dot => {
+                if (Number(dot.id) !== index) {
+                    dot.style["backgroundColor"] = "dimgray"
+                } else {
+                    dot.style["backgroundColor"] = "white"
                 }
+            })
+            if (index >= CAROUSEL_MAX_IMAGES - 1) {
+                console.log("zero")
+                setIndex(0)
+            } else {
+                console.log("increment")
+                setIndex(index + 1)
             }
-            timeout()
-        }, 3000)
+        }
     }
+
+    timerRef.current = setTimeout(
+        () => {
+            clearTimeout(timerRef.current)
+            changeApartment()
+        },
+
+        2000
+    )
 
     return (
         <div className="carousel-container">
@@ -83,6 +101,25 @@ const Carousel = () => {
                     ? `${currentApartment.city} | ${currentApartment.address} | ${currentApartment.price}€`
                     : null}
             </span>
+            <div
+                onClick={() => {
+                    clearTimeout(timerRef)
+                    changeApartment()
+                }}
+                className="right-arrow"
+            >
+                <img src={arrow} alt="right arrow" />
+            </div>
+            <div
+                onClick={() => {
+                    clearTimeout(timerRef)
+                    setIndex(0)
+                    changeApartment()
+                }}
+                className="left-arrow"
+            >
+                <img src={arrow} alt="left-arrow" />
+            </div>
         </div>
     )
 }
